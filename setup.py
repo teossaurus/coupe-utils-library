ca5a1218390c4e3f3fec20e024c3f6ec3aa91fa7
Teo Soares <teosoares@teos-mbp.lan>
2024-09-13 17:46:49 -0400

    Editing setup.py to fix issue pushing to PyPi

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
   author_email = 'hello@teo.works',      # Type in your E-Mail
   url = 'https://github.com/teossaurus/coupe-utils-library',   # Provide either the link to your github or to your website
   keywords = [],   # Keywords that define your package best
+  long_description= 'Library with utilities for projects.',
   install_requires=[
       'annotated-types',
       'anthropic',
@@ -85,15 +86,5 @@ setup(
       'weaviate-client',
       'websocket-client',
       'wsproto',
-  ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
   ],
 )
