a3fa982a21b01c499ff6e899233198ec7de3de42
Teo Soares <teosoares@teos-mbp.lan>
2024-10-04 09:26:15 -0400

    Updating workflow file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="coupeutils",  # How you named your package folder (MyLib)
     packages=find_packages(),  # Chose the same as "name"
-    version="0.0.1",  # Start with a small number and increase it with every change you make
+    version="0.0.2",  # Start with a small number and increase it with every change you make
     license="MIT",  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description="Library with utilities for projects.",  # Give a short description about your library
     author="Teo Soares",  # Type in your name
